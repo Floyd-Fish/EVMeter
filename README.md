2caada9fa8a59e2b17a98f380265dc2d8bb77de2
Floyd-Fish <summer200816@outlook.com>
2020-03-17 14:40:20 +0800

    Add initial project frameworks

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# EVMeter
+# Overview
 An EV(Exposure Value) Meter measures EV parameters for cameras automatically. Also measures the Color Temperature.Designed to simplify MF controls on DIgital or Mechanical DSLRs
+
+## Hardware design
+Tools: KiCad  
+See dir: Hardware/Kicad_project   
+
+## Software design
+Tools: Clion + STM32CubeMx  
+MCU: STM32F070F6P6  
+See dir: Software/EVMeter.ioc  
---
+++ Software/cmake-build-debug/CMakeFiles/EVMeter.elf.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_spi.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_spi_ex.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Src/gpio.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Src/i2c.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Src/main.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Src/spi.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Src/stm32f0xx_hal_msp.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Src/stm32f0xx_it.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/Src/system_stm32f0xx.c.obj"
+  "CMakeFiles/EVMeter.elf.dir/startup/startup_stm32f070x6.s.obj"
+  "EVMeter.elf"
+  "EVMeter.elf.manifest"
+  "EVMeter.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/EVMeter.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
